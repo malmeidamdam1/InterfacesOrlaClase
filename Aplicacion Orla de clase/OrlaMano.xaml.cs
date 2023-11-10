e54be0a9849a3aaf5a35abcccd6cbb75fc6697c6
Midgard <malmeidamdam1@gmail.com>
2023-11-10 09:31:07 +0100

    Subiendo Proyecto a git

---
+++ Aplicacion Orla de clase/OrlaMano.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Aplicacion_Orla_de_clase
+{
+    /// <summary>
+    /// Lógica de interacción para OrlaMano.xaml
+    /// </summary>
+    public partial class OrlaMano : Window
+    {
+        public OrlaMano()
+        {
+            InitializeComponent();
+        }
+        private void MouseEnter11(object sender, MouseEventArgs e)
+        {
+            LabelPuesto11.Text = "Aitor puesto 1.1";
+        }
+
+        private void MouseLeave11(object sender, MouseEventArgs e)
+        {
+            LabelPuesto11.Text = " ";
+        }
+
+        private void MouseClick11(object sender, RoutedEventArgs e)
+        {
+            LabelPuesto11.Text = "email_aitorC@educacion.navarra.es";
+        }
+
+        private void MouseClick12(object sender, RoutedEventArgs e)
+        {
+            LabelPuesto11.Text = "email_aitorC@educacion.navarra.es";
+        }
+
+        private void Peronsa11_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Profile win2 = new Profile("Aitor", "Cuadrado", "aitor@gmail.com", "src/Aitor.png");
+            win2.Show();
+        }
+    }
+}
---
+++ Aplicacion Orla de clase/Puesto.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Aplicacion_Orla_de_clase
+{
+    /// <summary>
+    /// Lógica de interacción para Puesto.xaml
+    /// </summary>
+    public partial class Puesto : Window
+    {
+        public Puesto()
+        {
+            InitializeComponent();
+        }
+
+       
+        public static DependencyProperty NombreProperty = DependencyProperty.Register("Apellidos", typeof(string), typeof(Puesto),
+            new PropertyMetadata(string.Empty));
+
+        public string Nombre
+        {
+            get { return (string)GetValue(NombreProperty); }
+            set { SetValue(NombreProperty, value); }
+        }
+
+        //Propiedad2
+        public static DependencyProperty ApellidosProperty = DependencyProperty.Register("Nombre", typeof(string), typeof(Puesto),
+            new PropertyMetadata(string.Empty));
+
+        public string Apellidos
+        {
+            get { return (string)GetValue(ApellidosProperty); }
+            set { SetValue(ApellidosProperty, value); }
+        }
+
+        //Propiedad3
+        public static DependencyProperty EmailProperty = DependencyProperty.Register("Email", typeof(string), typeof(Puesto),
+            new PropertyMetadata(string.Empty));
+
+        public string Email
+        {
+            get { return (string)GetValue(EmailProperty); }
+            set { SetValue(EmailProperty, value); }
+        }
+
+        //Propiedad4
+        public static DependencyProperty FotoProperty = DependencyProperty.Register("Foto", typeof(string), typeof(Puesto),
+             new PropertyMetadata(string.Empty));
+
+        public string Foto
+        {
+            get { return (string)GetValue(FotoProperty); }
+            set { SetValue(FotoProperty, value); }
+        }
+
+        private void Persona_MouseEnter(object sender, MouseEventArgs e)
+        {
+            LabelPuesto.Text = Nombre + " " + Apellidos;
+        }
+
+        private void Persona_MouseLeave(object sender, MouseEventArgs e)
+        {
+            LabelPuesto.Text = "";
+
+        }
+
+        private void Persona_Click(object sender, RoutedEventArgs e)
+        {
+            LabelPuesto.Text = Email;
+        }
+
+        private void Persona_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Profile win2 = new Profile(Nombre, Apellidos, Email, Foto);
+            win2.Show();
+        }
+    }
+}
